e79509deb5294662d47bf00c55d17bcec8c5cad3
guptan <nguptaa183@gmail.com>
2017-12-02 22:25:02 +0545

    changes

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import jarvis
 
 app = Flask(__name__)
 
-
+# ACCESS_TOKEN = "EAAUa1N8qPX4BAE4G0mLSpmi7yZAam42T9ssMsPnhAZCW3C18TddOBgaKLZC5jT84UPZCa580ZB51i3czygl4Cm4MfWZBZBSgGZCKxrRqvMlvYEC6wVPha9DcUd5teWonedBU6LrFZCvZANArhAr4nWZBTa2BZAUAp4cduOZCJnqtBZBvfA9QZDZD"
 
 def reply(user_id, msg):
     print msg
@@ -16,7 +16,7 @@ def reply(user_id, msg):
         "recipient": {"id": user_id},
         "message": {"text": msg}
     }
-
+    # resp = requests.post("https://graph.facebook.com/v2.6/me/messages?access_token=" + ACCESS_TOKEN, json=data)
     print(resp.content)
 
 @app.route('/', methods=['GET'])
